94c7fa7839091353e795df88e552bb2c6a30caa2
DmitryDadeco <dimdaxdadeco@mail.ru>
2018-06-11 21:48:57 +0300

    scss & jsx

--- src/components/App/index.jsx
+++ src/components/App/index.jsx
@@ -1,9 +1,11 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+
+import s from "./styles.scss";
 
 class App extends Component {
     render() {
         return (
-            <div>
+            <div className={s.container}>
                 <h1>My React App!</h1>
             </div>
         );
--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,17 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const autoprefixer = require('autoprefixer');
 
+const postCssLoader = {
+    loader: 'postcss-loader',
+    options: {
+        plugins: [
+            autoprefixer({
+                browsers: ['last 2 versions']
+            })
+        ],
+    }
+};
 
 module.exports = {
     entry: './src/index.js',
@@ -11,15 +22,15 @@ module.exports = {
     module: {
         rules: [
             {
-                test: /\.js$/,
+                test: /\.(js|jsx)$/,
                 exclude: /node_modules/,
                 use: {
                     loader: 'babel-loader'
                 },
             },
             {
-                test: /\.css$/,
-                use: ['style-loader', 'css-loader']
+                test: /\.scss$/,
+                use: ['style-loader', 'css-loader', postCssLoader, 'sass-loader']
             }
         ]
     },
@@ -27,5 +38,9 @@ module.exports = {
         new HtmlWebpackPlugin({
             template: './src/index.html'
         })
-    ]
+    ],
+    devtool: 'source-map',
+    resolve: {
+        extensions: ['.js', '.jsx'],
+    }
 };
